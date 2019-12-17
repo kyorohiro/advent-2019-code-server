8c2f33d16d1b944059a9e9c003725f3fc9aa179d
kyorohiro <kyorohiro@gmail.com>
2019-12-18 00:54:10 +0900

    worklog

--- remote_cs01/for_aws/main.py
+++ remote_cs01/for_aws/main.py
@@ -57,9 +57,78 @@ def delete_gateway():
         res = ec2client.delete_internet_gateway(InternetGatewayId=gateway['InternetGatewayId'])
         print("{}".format(res))
 
+def create_subnet(vpc_id:str):
+    print(">>> CREATE SUBNET")
+    res = ec2client.create_subnet(CidrBlock='10.1.0.0/24',VpcId=vpc_id)
+    print("{}".format(res))
+    subnet_id = res['Subnet']['SubnetId']
+    attach_tag(subnet_id)
+    return subnet_id
+
+def delete_subnet():
+    print(">> Delete subnet")
+    res = ec2client.describe_subnets(Filters=[{"Name":"tag:Name","Values":[instance_name]}])
+    print("{}".format(res))
+    for subnet in res["Subnets"]:
+        res = ec2client.delete_subnet(SubnetId=subnet['SubnetId'])
+        print("{}".format(res))
+
+def create_security_group():
+    print(">>> CREATE SECURITY GROUP")
+    res = ec2client.create_security_group(Description="AdventCodeServer",GroupName=instance_name)
+    print("{}".format(res))
+    group_id = res['GroupId']
+    attach_tag(group_id)
+    return group_id
+
+def delete_security_group():
+    res = ec2client.describe_security_groups(Filters=[{"Name":"tag:Name","Values":[instance_name]}])
+    print("{}".format(res))
+    for sg in res['SecurityGroups']:
+        res = ec2client.delete_security_group(GroupId=sg["GroupId"])
+        print("{}".format(res))
+
+def create_security_group_ingress():
+        print(">>>> CREATE SECURITY GROUP INGRESS")
+        res = ec2client.authorize_security_group_ingress(
+                GroupName=instance_name, IpPermissions=[
+                    {
+                        'IpProtocol': 'tcp',
+                        'FromPort': 8443,
+                        'ToPort': 8443,
+                        'IpRanges':[
+                            {'CidrIp': '0.0.0.0/0', 'Description' : '8443'}
+                        ]
+                    },
+                    {
+                        'IpProtocol': 'tcp',
+                        'FromPort': 8080,
+                        'ToPort': 8080,
+                        'IpRanges':[
+                            {'CidrIp': '0.0.0.0/0', 'Description' : '8080'}
+                        ]
+                    },
+                    {
+                        'IpProtocol': 'tcp',
+                        'FromPort': 22,
+                        'ToPort': 22,
+                        'IpRanges':[
+                            {'CidrIp': '0.0.0.0/0', 'Description' : '8080'}
+                        ]
+                    },
+                ])
+        print("{}".format(res))
+
 if __name__ == "__main__":
     vpc_id:str = create_vpc()
     gateway_id:str = create_gateway(vpc_id)
+    subnet_id = create_subnet(vpc_id)
+    group_id = create_security_group()
+    create_security_group_ingress()
+    #create_instance()
+    #delete_instance()
+    delete_security_group()
+    delete_subnet()
     delete_gateway()
     delete_vpc()
 

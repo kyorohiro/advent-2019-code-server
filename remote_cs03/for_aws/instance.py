22a817b5f0ee581527c1702da014693ea08c515f
kyorohiro <kyorohiro@gmail.com>
2019-12-21 21:14:47 +0900

    ..

---
+++ remote_cs03/for_aws/instance.py
@@ -0,0 +1,91 @@
+import boto3
+from boto3_type_annotations import ec2
+from botocore.exceptions import ClientError
+from typing import Dict, List 
+import time
+import network
+
+ec2client:ec2.Client = boto3.client("ec2")
+
+class CodeServerInsrance:
+
+    def __init__(self, ec2_client: ec2.Client, project_name="advent-code-server", instance_type='t2.micro', image_id="ami-0cd744adeca97abb1"):
+        self._ec2_client:ec2.Client = ec2_client
+        self._project_name:str = project_name
+        self._instance_type = instance_type
+        self._pem_data:str = ""
+        self._image_id=image_id
+
+    def create_pem(self):
+        print(">>> CREATE KEY_PAIR")
+        res = ec2client.create_key_pair(KeyName=self._project_name)
+        print("{}".format(res))
+        self._pem_data =res['KeyMaterial']
+
+    def delete_pem(self):
+        print(">>>> DELETE KeyPair")
+        ec2client.delete_key_pair(KeyName=self._project_name)
+
+    def create_instance(self, subnet_id:str, group_id:str):
+        print(">>>> CREATE INSTANCE")
+        res = ec2client.run_instances(ImageId=self._image_id,
+            InstanceType=self._instance_type,
+            MinCount=1,MaxCount=1,KeyName=self._project_name,
+            TagSpecifications=[{
+                'ResourceType': 'instance',
+                'Tags': [{
+                    'Key': 'Name',
+                    'Value': self._project_name
+                }]
+            }],
+            NetworkInterfaces=[{"SubnetId":subnet_id, 'AssociatePublicIpAddress': True, 'DeviceIndex':0,'Groups': [group_id]}]
+            )
+        print("{}".format(res))
+
+        return self._project_name
+
+
+    def delete_instance(self):
+        print(">>>> ec2client.describe_instances")
+        res = ec2client.describe_instances(
+            Filters=[{"Name":"tag:Name","Values":[self._project_name]}]
+            )
+        print("{}".format(res))
+
+        print(">>>> DELETE Instance")
+        for reservation in res['Reservations']:
+            for instance in reservation['Instances']:
+                print("------{}".format(instance))
+                instance_id = instance['InstanceId']
+                print(">>>> {}".format(instance_id))
+                res = ec2client.terminate_instances(InstanceIds=[instance_id])
+
+        print("{}".format(res))
+
+    def wait_instance_is_terminated(self):
+        while(True):
+            res = ec2client.describe_instances(
+                Filters=[{"Name":"tag:Name","Values":[self._project_name]}]
+                )
+            terminated = False
+            for reservation in res['Reservations']:
+                for instance in reservation['Instances']:
+                    instance_state = instance['State']['Name']
+                    print("------{}".format(instance_state))
+                    if instance_state != 'terminated':
+                        terminated = True
+            if terminated == False:
+                break
+            time.sleep(6)
+
+
+'''
+{
+    "vpc_id":vpc_id,
+    "gateway_id":gateway_id,
+    "subnet_id":subnet_id,
+    "group_id":group_id,
+    "route_table_id":route_table_id
+}
+'''
+
---
+++ remote_cs03/for_aws/main.py
@@ -0,0 +1,68 @@
+
+import boto3
+from boto3_type_annotations import ec2
+from botocore.exceptions import ClientError
+from typing import Dict, List 
+import time
+from network import CodeServerNetwork
+from instance import CodeServerInsrance
+
+
+def create_network(network:CodeServerNetwork):
+    print(">>> Create")
+    vpc_id:str = network.create_vpc()
+    gateway_id:str = network.create_gateway(vpc_id)
+    subnet_id = network.create_subnet(vpc_id)
+    group_id = network.create_security_group(vpc_id)
+    network.create_security_group_ingress(group_id)
+    route_table_id = network.create_route_table(vpc_id)
+    network.create_route(route_table_id, gateway_id)
+    network.associate_route_table(route_table_id, subnet_id)
+    return  {
+        "vpc_id":vpc_id,
+        "gateway_id":gateway_id,
+        "subnet_id":subnet_id,
+        "group_id":group_id,
+        "route_table_id":route_table_id
+    }
+
+
+def delete_network(network:CodeServerNetwork):
+    print(">>> Delete")
+    res = network.list_vpc()
+    print("{}".format(res))
+
+    # delete at vpc_id
+    for vpc in res["Vpcs"]:
+        vpc_id=vpc['VpcId']
+        network.delete_route_table(vpc_id)
+        network.delete_security_group(vpc_id)
+        network.delete_subnet(vpc_id) 
+        network.delete_gateway(vpc_id)
+        #delete_vpc(vpc_id)
+
+    # delete at instance name
+    network.delete_route_table()
+    network.delete_security_group()
+    network.delete_subnet()
+    network.delete_gateway()
+    network.delete_vpc()
+
+def create_intance(instance:CodeServerInsrance, network:CodeServerNetwork):
+    instance.create_pem()
+    instance.create_instance(subnet_id=network.subnet_id, group_id=network.group_id)
+
+def delete_intance(instance:CodeServerInsrance):
+    instance.delete_pem()
+    instance.delete_instance()
+
+if __name__ == "__main__":
+    ec2_client:ec2.Client = boto3.client("ec2")
+    network:CodeServerNetwork = CodeServerNetwork(ec2_client, project_name="advent-code-server", ports=[22,8443,8080], vpc_cidr_block='10.1.0.0/16', subnet_cidr_block='10.1.0.0/24')
+    instance:CodeServerInsrance = CodeServerInsrance(ec2_client, project_name="advent-code-server", instance_type='t2.micro', image_id="ami-0cd744adeca97abb1")
+    create_network(network)
+    create_intance(instance, network)
+    delete_intance(instance)
+    instance.wait_instance_is_terminated()
+    delete_network(network)
+
